c0d4166d45a440e93ad5bf672fc688629d9f9a79
piippy <38547426+piippy@users.noreply.github.com>
2018-08-31 08:04:42 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,11 +3,11 @@ $access_token = 'EAAhGuNGZCkpcBABPnOz6f7entM9lg3ZAPFqhuFK2AsEuQLKF2ZCshZCHxyxr4Z
 
 /* validate verify token needed for setting up web hook */
 if (isset($_GET['hub_verify_token'])) {
-if ($_GET['hub_verify_token'] === $access_token) {
-echo $_GET['hub_challenge'];
-return;
-} else {
-echo 'Invalid Verify Token';
-return;
-}
+  if ($_GET['hub_verify_token'] === $access_token) {
+    echo $_GET['hub_challenge'];
+    return;
+  } else {
+    echo 'Invalid Verify Token';
+    return;
+  }
 }
