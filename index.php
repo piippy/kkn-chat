19bf8d9a8f9cb8398e6a8bba213ae597ce5248cd
piippy <38547426+piippy@users.noreply.github.com>
2018-08-31 08:14:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -11,3 +11,31 @@ if (isset($_GET['hub_verify_token'])) {
     return;
   }
 }
+
+/* receive and send messages */
+
+$input = json_decode(file_get_contents('php://input'), true);
+if (isset($input['entry'][0]['messaging'][0]['sender']['id'])) {
+  $sender = $input['entry'][0]['messaging'][0]['sender']['id'];
+  $message = $input['entry'][0]['messaging'][0]['message']['text'];
+  $url = 'https://graph.facebook.com/v2.6/me/messages?access_token=' . $access_token;
+  /*initialize curl*/
+  $ch = curl_init($url);
+  /*prepare response*/
+  $jsonData = '{
+    "recipient" : {
+      "id" : "' . $sender . '"
+    },
+    "message" : {
+      "text" : "OK"
+    }
+  }';
+  
+  /* curl setting to send a json post data */
+  curl_setopt($ch, CURLOPT_POST, 1);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+  if (!empty($message)) {
+    $result = curl_exec($ch); // user will get the message
+  }
+}
