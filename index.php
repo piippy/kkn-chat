55ea297d07a54171f1bf57be1455a27e6ba751ba
piippy <38547426+piippy@users.noreply.github.com>
2018-08-31 08:03:57 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+$access_token = 'EAAhGuNGZCkpcBABPnOz6f7entM9lg3ZAPFqhuFK2AsEuQLKF2ZCshZCHxyxr4ZBO42IT176TiQCPsmCZCLkauoV9b2B2E9tek60s7qR78GuuiBhs3yfdxBNVe1cThH6qWuFVM4NX0pfJvqY2h5QqABE9TAanEBYS9mQkdsMZBWnDAZDZD';
+
+/* validate verify token needed for setting up web hook */
+if (isset($_GET['hub_verify_token'])) {
+if ($_GET['hub_verify_token'] === $access_token) {
+echo $_GET['hub_challenge'];
+return;
+} else {
+echo 'Invalid Verify Token';
+return;
+}
+}
