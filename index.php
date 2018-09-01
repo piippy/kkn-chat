482396c4a05c979b92fed95d8473b7d6becb403c
Pii <pattaragorn.kh@gmail.com>
2018-09-01 13:40:51 +0700

    working

--- index.php
+++ index.php
@@ -36,49 +36,45 @@ Kerry เก็บเงินปลายทาง - 100บาท
   )
 );
 
-function match_response($input) {
-  for ($i = 0, $ii = count($logics); $i < $ii; $i++) {
-    for ($j = 0, $jj = count($logics[$i]['matchers']); $j < $jj; $j++) {
-      $matcher = $logics[$i]['matchers'][$j];
+function get_response($text) {
+  $msg = ":);text:$text;";
+  foreach ($logics as $logic) {
+    foreach ($logic['matchers'] as $matcher) {
+      $msg .= "type:{$matcher['type']};";
       switch ($matcher['type']) {
         case 'exact':
-          if ($matcher['value'] == $input)
-            return $logics[$i]['return'];
+          if ($matcher['value'] == $text)
+            $msg .= 'return:' . $logic['return'] . ';';
           break;
-
         default:
           // code...
           break;
       }
     }
   }
-  return "Your message is $input.";
+  return $msg;
 }
+
+/*initialize curl*/
+$ch = curl_init("https://graph.facebook.com/v2.6/me/messages?access_token=$access_token");
 
 /* receive and send messages */
 $input = json_decode(file_get_contents('php://input'), true);
-if (isset($input['entry'][0]['messaging'][0]['sender']['id'])) {
-  $sender = $input['entry'][0]['messaging'][0]['sender']['id'];
-  $message = $input['entry'][0]['messaging'][0]['message']['text'];
-  $url = 'https://graph.facebook.com/v2.6/me/messages?access_token=' . $access_token;
-  /*initialize curl*/
-  $ch = curl_init($url);
-  /*prepare response*/
-  $resp = array(
-    'recipient' => array(
-      'id' => $sender
-    ),
-    'message' => array(
-      // 'text' => match_response($input['entry'][0]['messaging'][0]['message']['text'])
-      'text' => json_encode($input)
-    )
-  );
+foreach ($input['entry'] as $entry) {
+  foreach ($entry['messaging'] as $messaging) {
+    $sender = $messaging['sender']['id'];
+    $text = $messaging['message']['text'];
 
-  /* curl setting to send a json post data */
-  curl_setopt($ch, CURLOPT_POST, 1);
-  curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($resp));
-  curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-  if (!empty($message)) {
+    /*prepare response*/
+    $resp = array(
+      'recipient' => array('id' => $sender),
+      'message' => array('text' => get_response($text))
+    );
+
+    /* curl setting to send a json post data */
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($resp));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
     $result = curl_exec($ch); // user will get the message
   }
 }
