70829b59792c2d9885353276fe48f692b8e57817
Pii <pattaragorn.kh@gmail.com>
2018-09-01 14:35:21 +0700

    wokring

--- index.php
+++ index.php
@@ -26,25 +26,34 @@ $logics = array(
     "return" =>
 "ค่าบริการการจัดส่ง สั่งกี่คู่ราคาส่งก็เท่ากันครับ
 
-ลงทะเบียนธรรมดา - 20บาท
-EMS - 60บาท
-Kerry - 80บาท
-Kerry เก็บเงินปลายทาง - 100บาท
+จัดส่งด้วย ไปรษณีย์ แบบEMS(โอนเงิน): 60บาท
+จัดส่งด้วย Kerry แบบเก็บเงินปลายทาง: 100บาท
 
 ค่าจัดส่งจะเป็นตามนี้ครับผม"
+  ),
+  array(
+    "matchers" => array(
+      array("type" => "exact", "value" => "สอบถามชุดธุดงค์")
+    ),
+    "return" =>
+"รองเท้าแตะชุดธุดงค์ มี4แบบตามภาพครับ
+ราคาคู่ละ 540บาท
+มีไซส์ 40-45ครับ
+https://scontent.fbkk2-6.fna.fbcdn.net/v/t1.15752-0/p480x480/40545218_2108115272770277_6181129212267593728_n.jpg?_nc_cat=0&oh=60cc012ab59485274c48e66c3d0e62da&oe=5C33C299
+https://scontent.fbkk2-6.fna.fbcdn.net/v/t1.15752-0/p480x480/40463218_285961015553297_675509540032086016_n.jpg?_nc_cat=0&oh=a2f1f76c6b66d68fac986d3ab26140f4&oe=5BFC460D
+https://scontent.fbkk2-6.fna.fbcdn.net/v/t1.15752-0/p480x480/40448931_1833222963440838_956808118140928000_n.jpg?_nc_cat=0&oh=e49875cbe1638aecf4450072f732b1de&oe=5C33BA45
+https://scontent.fbkk2-6.fna.fbcdn.net/v/t1.15752-0/p480x480/40573170_706525106348906_5674443755625644032_n.jpg?_nc_cat=0&oh=bcc4b956ec2b65a2cb724fa1dba3054b&oe=5BF46B70"
   )
 );
 
 function get_response($text) {
   global $logics;
-  $msg = ":);text:$text;";
   foreach ($logics as $logic) {
     foreach ($logic['matchers'] as $matcher) {
-      $msg .= "type:{$matcher['type']};";
       switch ($matcher['type']) {
         case 'exact':
           if ($matcher['value'] == $text)
-            $msg .= 'return:' . $logic['return'] . ';';
+            return $logic['return'];
           break;
         default:
           // code...
@@ -52,7 +61,6 @@ function get_response($text) {
       }
     }
   }
-  return $msg;
 }
 
 /*initialize curl*/
@@ -66,14 +74,16 @@ foreach ($input['entry'] as $entry) {
     $text = $messaging['message']['text'];
 
     /*prepare response*/
-    $resp = array(
-      'recipient' => array('id' => $sender),
-      'message' => array('text' => get_response($text))
-    );
+    $msg = get_response($text);
+    if (!$sender || !$msg)
+      continue;
 
     /* curl setting to send a json post data */
     curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($resp));
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode(array(
+      'recipient' => array('id' => $sender),
+      'message' => array('text' => $msg)
+    )));
     curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
     $result = curl_exec($ch); // user will get the message
   }
