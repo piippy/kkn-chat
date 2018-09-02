41fe44ac2b3348cf8281470c073ad71be6bc6ebb
Pii <pattaragorn.kh@gmail.com>
2018-09-02 18:34:52 +0700

    working

--- index.php
+++ index.php
@@ -40,10 +40,8 @@ foreach ($input['entry'] as $entry) {
     if (empty($sender))
       continue;
     $responses = get_responses($messaging['message']['text']);
-    // if (empty($responses))
-    //   continue;
-    bot_answer(array('text'=>json_encode($responses)), $sender);
-    bot_answer(array('text'=>$messaging['message']['text']), $sender);
+    if (empty($responses))
+      continue;
     foreach($responses as $resp)
       bot_answer($resp, $sender);
   }
